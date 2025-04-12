1fd759fcee4ee7f590cfbcc54528babda4244482
Wladison-Maciel <franciscowladison97@gmail.com>
2025-04-12 18:58:37 -0300

    feat: Adicionando rotas para companion

--- apihospitalar/src/routes.js
+++ apihospitalar/src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import patients from "./app/controllers/PatientsController.js"
+import companion from "./app/controllers/CompanionsController.js"
 const routes = new Router(); 
 
 // Rotas dos Pacientes
@@ -8,5 +9,12 @@ routes.get("/patients/:id" , patients.show );
 routes.post("/patients" , patients.create );
 routes.patch("/patients/:id" , patients.update );
 routes.delete("/patients/:id" , patients.destroy );
+
+// Rotas do Acompanhante
+routes.get("/patient/:patiendId/companion", companion.index);
+routes.get("/patient/:patiendId/companion/:id", companion.show);
+routes.create("/patient/:patiendId/companion", companion.create);
+routes.get("/patient/:patiendId/companion/:id", companion.update);
+routes.get("/patient/:patiendId/companion/:id", companion.destroy);
 
 export default routes;

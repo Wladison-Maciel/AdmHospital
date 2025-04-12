1f013a0b09f03865b64e88f84d4ae965655fcfba
Wladison-Maciel <franciscowladison97@gmail.com>
2025-04-12 19:45:32 -0300

    feat: Adicionando dotenv para variáveis de ambiente

--- apihospitalar/src/config/database.js
+++ apihospitalar/src/config/database.js
@@ -1,13 +1,15 @@
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+
 module.exports = {
-    dialect: "postgres", // Define o banco de dados como PostgreSQL
-    host: "localhost", // Define o servidor do banco de dados (neste caso, local)
-    username: "postgres", // Usuário do banco de dados
-    password: "3769", // Senha do banco de dados
-    database: "AdmHospital", // Nome do banco de dados
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 
     define: {
-        timestamps: true, // Habilita a criação automática das colunas "createdAt" e "updatedAt"
-        underscored: true, // Define que os nomes das tabelas e colunas usarão snake_case
-        underscoredAll: true, // Aplica o padrão snake_case para nomes de tabelas e colunas automaticamente
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
     },
 };

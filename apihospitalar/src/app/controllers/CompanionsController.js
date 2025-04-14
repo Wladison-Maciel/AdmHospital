bba65aba95e2b21914ae36a78c76326904c94a39
Wladison-Maciel <franciscowladison97@gmail.com>
2025-04-14 19:38:29 -0300

    docs: Adicionando documentação para melhor entendimento

--- apihospitalar/src/app/controllers/CompanionsController.js
+++ apihospitalar/src/app/controllers/CompanionsController.js
@@ -1,4 +1,3 @@
-import { Op } from "sequelize";
 import Companion from "../models/Companion";
 import Patient from "../models/Patient";
 
@@ -6,10 +5,12 @@ class CompanionsController {
     async index(req, res) {
 
     }
+    // Método para buscar um Acompanhante
     async show(req, res) {
         try {
+            // Recebendo id e patient_id da URL
             const { id, patient_id } = req.params;
-
+            // Verificando se id e patient_id são válidos
             if (isNaN(id)) {
                 return res.status(400).json({ error: "ID companion invalid" });
             }
@@ -17,8 +18,9 @@ class CompanionsController {
             if (isNaN(patient_id)) {
                 return res.status(404).json({ error: "ID patient invalid" })
             }
-
+            // Relizando busca por companion
             const companion = await Companion.findOne({
+                // Incluindo o patient para melhor informação
                 include: [
                     {
                         model: Patient,
@@ -27,17 +29,18 @@ class CompanionsController {
                     }
                 ],
                 where: {
-                    id,          
+                    id,
                     patient_id
                 },
+                // Excluindo patient_id 
                 attributes: ["id", "name", "phone", "cpf"],
-                order: [["id", "ASC"]],
+                order: [["id", "ASC"]], // Ordenando os ID de forma crescente
             });
-
+            // Retornando mensagem de erro em caso de falha
             if (!companion) {
                 return res.status(404).json({ error: "Companion not found" })
             }
-
+            // Retornando companion em caso de sucesso
             return res.status(200).json({
                 success: true,
                 data: companion
